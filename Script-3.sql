765731908b7ff6a769987a7fe2d745e3ff50eefe
Rhett Harrison <reharri7@asu.edu>
2023-01-30 19:11:36 -0800

    Changed boolean data types

--- Script-3.sql
+++ Script-3.sql
@@ -18,50 +18,50 @@
 create table if not exists agenda_type
 (
     agenda_type_id    int         not null
-    primary key,
+        primary key,
     agenda_type_value varchar(35) null
-    );
+);
 
 create table if not exists bike_type
 (
     bike_type_id    int not null
-    primary key,
+        primary key,
     bike_type_value int null
 );
 
 create table if not exists bike
 (
     bike_id       int         not null
-    primary key,
+        primary key,
     brand         varchar(30) null,
     type          int         not null,
     license_plate varchar(30) not null,
     vin           varchar(30) not null,
-    broken        tinyint(1)  null,
+    broken        tinyint  null,
     cc            int         null,
     constraint bike_bike_type_bike_type_id_fk
-    foreign key (type) references bike_type (bike_type_id)
-    );
+        foreign key (type) references bike_type (bike_type_id)
+);
 
 create table if not exists classroom
 (
     classroom_id int        not null
-    primary key,
+        primary key,
     capacity     int        not null,
-    available    tinyint(1) null
-    );
+    available    tinyint null
+);
 
 create table if not exists course_type
 (
     course_type_id    int         not null
-    primary key,
+        primary key,
     course_type_value varchar(30) not null
-    );
+);
 
 create table if not exists course
 (
     course_id          int          not null
-    primary key,
+        primary key,
     course_name        varchar(30)  null,
     course_type        int          not null,
     capacity           int          null,
@@ -69,30 +69,30 @@ create table if not exists course
     cost               int          null,
     course_date        date         not null,
     constraint course_course_type_course_type_id_fk
-    foreign key (course_type) references course_type (course_type_id)
-    );
+        foreign key (course_type) references course_type (course_type_id)
+);
 
 create table if not exists person
 (
     person_id  int          not null
-    primary key,
+        primary key,
     full_name  varchar(30)  null,
     address    varchar(150) null,
     date_birth date         null,
     phone      varchar(10)  null
-    );
+);
 
 create table if not exists coach
 (
     person_id            int        not null,
     coach_id             int        not null
-    primary key,
-    classroom_certified  tinyint(1) null,
-    dirtbike_certified   tinyint(1) null,
-    streetbike_certified tinyint(1) null,
+        primary key,
+    classroom_certified  tinyint null,
+    dirtbike_certified   tinyint null,
+    streetbike_certified tinyint null,
     constraint coach_person_person_id_fk
-    foreign key (person_id) references person (person_id)
-    );
+        foreign key (person_id) references person (person_id)
+);
 
 create table if not exists problem
 (
@@ -102,62 +102,62 @@ create table if not exists problem
     description  varchar(150) null,
     cost         int          null,
     problem_id   int          not null
-    primary key,
+        primary key,
     constraint problem_bike_bike_id_fk
-    foreign key (bike_id) references bike (bike_id)
-    );
+        foreign key (bike_id) references bike (bike_id)
+);
 
 create table if not exists range_type
 (
     range_type_id    int         not null
-    primary key,
+        primary key,
     range_type_value varchar(30) not null
-    );
+);
 
 create table if not exists bike_range
 (
     range_id   int not null
-    primary key,
+        primary key,
     range_type int not null,
     constraint bike_range_range_type_range_type_id_fk
-    foreign key (range_type) references range_type (range_type_id)
-    );
+        foreign key (range_type) references range_type (range_type_id)
+);
 
 create table if not exists student
 (
     person_id  int not null,
     student_id int not null
-    primary key,
+        primary key,
     constraint student_person_person_id_fk
-    foreign key (person_id) references person (person_id)
-    );
+        foreign key (person_id) references person (person_id)
+);
 
 create table if not exists course_enrollment
 (
     course_enrollment_id int        not null
-    primary key,
+        primary key,
     course_id            int        not null,
     exercise_1_score     int        null,
     exercise_2_score     int        null,
     exercise_3_score     int        null,
     exercise_4_score     int        null,
     exercise_5_score     int        null,
-    passed               tinyint(1) null,
+    passed               tinyint null,
     written_score        int        null,
-    paid                 tinyint(1) not null,
+    paid                 tinyint not null,
     student_id           int        not null,
     constraint course_enrollment_course_course_id_fk
-    foreign key (course_id) references course (course_id),
+        foreign key (course_id) references course (course_id),
     constraint course_enrollment_student_student_id_fk
-    foreign key (student_id) references student (student_id)
-    );
+        foreign key (student_id) references student (student_id)
+);
 
 create table if not exists time_type
 (
     time_type_id    int         not null
-    primary key,
+        primary key,
     time_type_value varchar(30) not null
-    );
+);
 
 create table if not exists course_schedule
 (
@@ -166,21 +166,21 @@ create table if not exists course_schedule
     range_id           int  not null,
     course_date        date not null,
     course_schedule_id int  not null
-    primary key,
+        primary key,
     course_id          int  not null,
     time_type_id       int  not null,
     agenda_type        int  not null,
     constraint course_schedule_agenda_type_agenda_type_id_fk
-    foreign key (agenda_type) references agenda_type (agenda_type_id),
+        foreign key (agenda_type) references agenda_type (agenda_type_id),
     constraint course_schedule_bike_range_range_id_fk
-    foreign key (range_id) references bike_range (range_id),
+        foreign key (range_id) references bike_range (range_id),
     constraint course_schedule_classroom_classroom_id_fk
-    foreign key (classroom_id) references classroom (classroom_id),
+        foreign key (classroom_id) references classroom (classroom_id),
     constraint course_schedule_coach_coach_id_fk
-    foreign key (coach_id) references coach (coach_id),
+        foreign key (coach_id) references coach (coach_id),
     constraint course_schedule_course_course_id_fk
-    foreign key (course_id) references course (course_id),
+        foreign key (course_id) references course (course_id),
     constraint course_schedule_time_type_time_type_id_fk
-    foreign key (time_type_id) references time_type (time_type_id)
-    );
+        foreign key (time_type_id) references time_type (time_type_id)
+);
 
