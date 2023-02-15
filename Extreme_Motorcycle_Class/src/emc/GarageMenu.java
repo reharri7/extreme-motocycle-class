0db68b0319d08ca8abdcc1c7aee6428fb788dc39
Addison <72429964+addipls@users.noreply.github.com>
2023-02-15 03:02:50 -0800

    Create all methods and switch cases

--- Extreme_Motorcycle_Class/src/emc/GarageMenu.java
+++ Extreme_Motorcycle_Class/src/emc/GarageMenu.java
@@ -3,6 +3,7 @@ package emc;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GarageMenu {
@@ -19,7 +20,18 @@ public class GarageMenu {
                 case 2: viewBike(rs, stmt, conn, scanner); break;
                 case 3: editBike(rs, stmt, conn, scanner); break;
                 case 4: deleteBike(rs, stmt, conn, scanner); break;
-                
+                case 5: assignBike(rs, stmt, conn, scanner); break;
+                case 6: unassignBike(rs, stmt, conn, scanner); break;
+                case 7: bikeProblemReport(rs, stmt, conn, scanner); break;
+                case 8: bikeAssignmentReport(rs, stmt, conn, scanner); break;
+                case 9: createBike(rs, stmt, conn, scanner); break;
+                case 10: viewBike(rs, stmt, conn, scanner); break;
+                case 11: editBike(rs, stmt, conn, scanner); break;
+                case 12: deleteBikeProblem(rs, stmt, conn, scanner); break;
+                case 13: createBikeType(rs, stmt, conn, scanner); break;
+                case 14: viewBikeType(rs, stmt, conn, scanner); break;
+                case 15: editBikeType(rs, stmt, conn, scanner); break;
+                case 16: deleteBikeType(rs, stmt, conn, scanner); break;
                 case 0: return;
                 }
                 
@@ -28,6 +40,48 @@ public class GarageMenu {
                 scanner.next();
             }
         }
+    }
+
+    private void printMenu() {
+    }
+
+    private void deleteBikeType(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void editBikeType(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void viewBikeType(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void createBikeType(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void deleteBikeProblem(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void bikeAssignmentReport(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void bikeProblemReport(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void unassignBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void assignBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void deleteBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void editBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void viewBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void createBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
     }
 
 }
