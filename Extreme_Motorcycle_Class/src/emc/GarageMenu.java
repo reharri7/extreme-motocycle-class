e647c90a5a40e9682e15da6e63853a7d388437d3
addison <72429964+addipls@users.noreply.github.com>
2023-02-14 17:19:51 -0800

    Begin garage menu

--- Extreme_Motorcycle_Class/src/emc/GarageMenu.java
+++ Extreme_Motorcycle_Class/src/emc/GarageMenu.java
@@ -7,9 +7,27 @@ import java.util.Scanner;
 
 public class GarageMenu {
 
-    public void menu(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
-        // TODO Auto-generated method stub
+    public void menu(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) throws SQLException {
+        int selection;
         
+        while(true) {
+            printMenu();
+            try {
+                selection = scanner.nextInt();
+                switch (selection){
+                case 1: createBike(rs, stmt, conn, scanner); break;
+                case 2: viewBike(rs, stmt, conn, scanner); break;
+                case 3: editBike(rs, stmt, conn, scanner); break;
+                case 4: deleteBike(rs, stmt, conn, scanner); break;
+                
+                case 0: return;
+                }
+                
+            }catch (InputMismatchException ex){
+                System.out.println("Please enter an integer value between 0 and (TODO:)" );
+                scanner.next();
+            }
+        }
     }
 
 }
