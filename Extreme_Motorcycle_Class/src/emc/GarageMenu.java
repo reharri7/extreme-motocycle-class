06c0067ba10fdd8a49e44f74044b210260f3dc23
Addison <72429964+addipls@users.noreply.github.com>
2023-02-15 03:32:40 -0800

    All menu prints in GarageMenu

--- Extreme_Motorcycle_Class/src/emc/GarageMenu.java
+++ Extreme_Motorcycle_Class/src/emc/GarageMenu.java
@@ -2,6 +2,7 @@ package emc;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -24,12 +25,12 @@ public class GarageMenu {
                 case 6: unassignBike(rs, stmt, conn, scanner); break;
                 case 7: bikeProblemReport(rs, stmt, conn, scanner); break;
                 case 8: bikeAssignmentReport(rs, stmt, conn, scanner); break;
-                case 9: createBike(rs, stmt, conn, scanner); break;
-                case 10: viewBike(rs, stmt, conn, scanner); break;
-                case 11: editBike(rs, stmt, conn, scanner); break;
+                case 9: createBikeProblem(rs, stmt, conn, scanner); break;
+                case 10: viewBikeProblem(rs, stmt, conn, scanner); break;
+                case 11: editBikeProblem(rs, stmt, conn, scanner); break;
                 case 12: deleteBikeProblem(rs, stmt, conn, scanner); break;
                 case 13: createBikeType(rs, stmt, conn, scanner); break;
-                case 14: viewBikeType(rs, stmt, conn, scanner); break;
+                case 14: viewBikeTypes(rs, stmt, conn, scanner); break;
                 case 15: editBikeType(rs, stmt, conn, scanner); break;
                 case 16: deleteBikeType(rs, stmt, conn, scanner); break;
                 case 0: return;
@@ -42,46 +43,184 @@ public class GarageMenu {
         }
     }
 
+    
+
     private void printMenu() {
+        System.out.println("Garage Menu");
+        System.out.println("1. Create Bike");
+        System.out.println("2. View Bikes");
+        System.out.println("3. Edit Bike");
+        System.out.println("4. Delete Bike");
+        System.out.println("5. Assign Bike");
+        System.out.println("6. Unassign Bike");
+        System.out.println("7. Bike Problem Report");
+        System.out.println("8. Bike Assignment Report");
+        System.out.println("9. Create Bike Problem");
+        System.out.println("10. View Bike Problems");
+        System.out.println("11. Edit Bike Problem");
+        System.out.println("12. Delete Bike Problem");
+        System.out.println("13. Create Bike Type");
+        System.out.println("14. View Bike Types");
+        System.out.println("15. Edit Bike Type");
+        System.out.println("16. Delete Bike Type");
+        System.out.println("0. Main Menu");
     }
 
     private void deleteBikeType(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike type's ID: ");
+        int bikeTypeID = scanner.nextInt();
+
+
+        
     }
 
     private void editBikeType(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike type's ID: ");
+        int bikeTypeID = scanner.nextInt();
+
+        System.out.println("Please enter the attribute you wish to change: ");
+        String attribute = scanner.next();
+
+        System.out.println("Please enter the new value for the attribute: ");
+        String newValue = scanner.next();
+
+        /* Make a switch case to determine whether to cast the input to another type */
     }
 
-    private void viewBikeType(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    private void viewBikeTypes(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Here are all the bike types registered:");
+
     }
 
     private void createBikeType(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a name for the bike type between 1 and 10 characters: ");
+        String bikeTypeName = scanner.next();
     }
 
     private void deleteBikeProblem(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the problem ID: ");
+        int problemID = scanner.nextInt();
+
+
+    }
+
+    private void editBikeProblem(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike's ID: ");
+        int bikeID = scanner.nextInt();
+
+        System.out.println("Please enter the attribute you wish to change: ");
+        String attribute = scanner.next();
+
+        System.out.println("Please enter the new value for the attribute: ");
+        String newValue = scanner.next();
+
+        /* Make a switch case to determine whether to cast the input to another type */
+
+    }
+
+    private void viewBikeProblem(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike's ID: ");
+        int bikeID = scanner.nextInt();
+
+
+    }
+
+    private void createBikeProblem(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike's ID: ");
+        int bikeID = scanner.nextInt();
+
+        System.out.println("Please enter a date the problem occured: ");
+        String date = scanner.next();
+
+        System.out.println("Please enter a description of the problem between 1 and 150 characters: ");
+        String description = scanner.next();
+
+        System.out.println("Please enter a date to specify the repair date: ");
+        String repairDate = scanner.next();
+
+        System.out.println("Please enter a number for the cost of repair: ");
+        int cost = scanner.nextInt();
+
+
+
     }
 
     private void bikeAssignmentReport(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bikes ID: ");
+        int bikeID = scanner.nextInt();
+
+
     }
 
     private void bikeProblemReport(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike's ID: ");
+        int bikeID = scanner.nextInt();
+
+
     }
 
     private void unassignBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike's ID: ");
+        int bikeID = scanner.nextInt();
+
+
     }
 
     private void assignBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike's ID: ");
+        int bikeID = scanner.nextInt();
+        
+        System.out.println("Please a number for the course schedule's ID: ");
+        int courseScheduleID = scanner.nextInt();
+
+
     }
 
     private void deleteBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike's ID you wish to delete: ");
+        int bikeID = scanner.nextInt();
+
     }
 
     private void editBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Please enter a number for the bike's ID you wish to edit: ");
+        int bikeID = scanner.nextInt();
+
+        System.out.println("Please enter the attribute you wish to edit: ");
+        String attribute = scanner.next();
+
+        System.out.println("Please enter the new value for the attribute: ");
+        String newValue = scanner.next();
+
+        /* Make a switch case to determine whether to cast the input to another type */
+
+
     }
 
     private void viewBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
-    }
+        System.out.println("Here are all the bikes registered: ");
 
+    }
     private void createBike(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        System.out.println("Enter the bike's brand between 1-30 characters: ");
+        String brand = scanner.next();
+
+        System.out.println("Please enter a number for bike type: ");
+        int bikeType = scanner.nextInt();
+
+        System.out.println("Please enter the license plate between 1-30 characters: ");
+        String licensePlate = scanner.next();
+
+        System.out.println("Please enter the bike's VIN between 1-30 characters: ");
+        String vin = scanner.next();
+
+        System.out.println("Please enter a number to specify if the bike is broken: ");
+        int broken = scanner.nextInt();
+
+        System.out.println("Please enter a number to specify the bike's CCs: ");
+        int cc = scanner.nextInt();
+
+
     }
 
 }
