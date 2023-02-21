fcac57e8b3f34cc9c77fc8e3751754f013b8ef04
Addison <72429964+addipls@users.noreply.github.com>
2023-02-20 16:40:31 -0800

    Add printSet util

--- Extreme_Motorcycle_Class/src/emc/Utils.java
+++ Extreme_Motorcycle_Class/src/emc/Utils.java
@@ -1,7 +1,12 @@
 package emc;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
     public static LocalDate createLocalDate(int year, int month, int day) throws DateTimeException {
@@ -9,5 +14,69 @@ public class Utils {
             throw new DateTimeException("Invalid date entered.");
         }
         return LocalDate.of(year, month, day);
+    }
+
+    public static void printSet(ResultSet r) {
+        ResultSetMetaData rsmd = null;
+
+        // Will hold the number of columns in the result set
+        int columnCount = 0;
+
+        // Create a list to later hold the column names from metadata
+        List<String> columnNames = new ArrayList<String>();
+
+        // Load metadata from the result set and get column count while we're at it
+        try {
+            rsmd = r.getMetaData();
+            columnCount = rsmd.getColumnCount();
+        } catch (SQLException e) {
+            System.out.println("Error getting metadata");
+            e.printStackTrace();
+            return;
+        }
+
+        // Read the column names
+        // Do not modify the loop values: the result sets column index from 1 while a List index starts at 0.  The plus ones are to compensate for this.
+        for (int i = 0; i < columnCount; i++) {
+            try {
+                columnNames.add(rsmd.getColumnName(i + 1));
+            } catch (SQLException e) {
+                System.out.println("Error getting column name");
+                e.printStackTrace();
+                return;
+            }
+        }
+
+        // Print the column names
+        for (int i = 0; i < columnCount; i++) {
+            int ds = 0;
+            try {
+                ds = rsmd.getColumnDisplaySize(i + 1);
+                if (ds == 0) { // If a display size is unavailable, use the length of the column name plus 5 padding characters
+                    ds = columnNames.get(i).length() + 5;
+                }
+            } catch (SQLException e) {
+                System.out.println("Error getting column display size");
+                e.printStackTrace();
+            }
+            // Build the formatting using the display size
+            System.out.printf("%-" + ds + "s", columnNames.get(i));
+        }
+        System.out.println();
+
+        // Print the resultset
+        try {
+            while (r.next()) {
+                for (int i = 0; i < columnCount; i++) {
+                    rsmd.getColumnType(columnCount);
+                    int ds = rsmd.getColumnDisplaySize(i + 1);
+                    System.out.printf("%-" + ds + "s", r.getString(columnNames.get(i)));
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error printing result set");
+            e.printStackTrace();
+        }
     }
 }
