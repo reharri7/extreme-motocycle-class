7a9f87bbc3f1b2d391c86ec1c9544f9a8058b61a
Muhammad <mfateen@asu.edu>
2023-02-23 19:51:31 -0600

    Add Create Coach

--- Extreme_Motorcycle_Class/src/emc/StaffMenu.java
+++ Extreme_Motorcycle_Class/src/emc/StaffMenu.java
@@ -8,11 +8,7 @@ package emc;
  * Group 4
  */
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -70,13 +66,21 @@ public class StaffMenu {
         System.out.println("0. Main Menu");
     }
 
+    private void printSchedulesMenu() {
+        System.out.println("View Coach Schedule Sub Menu");
+        System.out.println("1. Specific Coach Schedule");
+        System.out.println("2. Availability Schedule");
+        System.out.println("0. Coach Menu");
+    }
+
     private void viewCoach(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
         System.out.println(": ");
+        String query = "SELECT coach.coach_id,person.full_name\n" +
+                "FROM coach,person \n" +
+                "WHERE coach.person_id=person.person_id;\n";
         try {
             stmt = conn.createStatement();
-            rs = stmt.executeQuery("SELECT coach.coach_id,person.full_name\n" +
-                    "FROM coach,person \n" +
-                    "WHERE coach.person_id=person.person_id;\n");
+            rs = stmt.executeQuery(query);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -85,6 +89,33 @@ public class StaffMenu {
     }
 
     private void viewCoachSchedule(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+        int choice;
+
+        while(true) {
+            printSchedulesMenu();
+            try {
+                choice = scanner.nextInt();
+                switch (choice) {
+                    case 1:
+                        coachWeeklySchedule(rs, stmt, conn, scanner);
+                        break;
+                    case 2:
+                        coachAvailabilitySchedule(rs, stmt, conn, scanner);
+                        break;
+                    case 0:
+                        return;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter an integer value between 0 and 2");
+                scanner.next();
+            }
+        }
+    }
+
+    private void coachAvailabilitySchedule(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    }
+
+    private void coachWeeklySchedule(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
     }
 
     private void unassignCoach(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
@@ -99,7 +130,59 @@ public class StaffMenu {
     private void editCoach(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
     }
 
-    private void createCoach(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) {
+    private void createCoach(ResultSet rs, Statement stmt, Connection conn, Scanner scanner) throws SQLException{
+        System.out.println("Name");
+        String full_name = scanner.next();
+
+        System.out.println("adress");
+        String address = scanner.next();
+
+        System.out.println("YYYY-MM-DD");
+        String birthDate  = scanner.next();
+        java.sql.Date sqlDate = java.sql.Date.valueOf(birthDate);
+
+        System.out.println("Phone");
+        String phone = scanner.next();
+
+        System.out.println("classroom_certified");
+        int classroom_certified = scanner.nextInt();
+
+        System.out.println("dirtbike_certified");
+        int dirtbike_certified = scanner.nextInt();
+
+        System.out.println("streetbike_certified");
+        int streetbike_certified = scanner.nextInt();
+
+        String query1 = "INSERT INTO person (full_name, address, date_birth, phone) VALUES (?, ?, ?, ?);";
+        String query2 = "INSERT INTO coach (person_id, classroom_certified, dirtbike_certified, streetbike_certified)\n" +
+                "VALUES ((SELECT person_id FROM person WHERE full_name=? AND address=? AND date_birth=? AND phone= ?), ?, ?, ?);";
+        PreparedStatement ps = null;
+        conn.setAutoCommit(false);
+        ps = conn.prepareStatement(query1);
+        ps.setString(1, full_name);
+        ps.setString(2, address);
+        ps.setDate(3, sqlDate);
+        ps.setString(4, phone);
+
+        PreparedStatement ps1 = null;
+        conn.setAutoCommit(false);
+        ps1 = conn.prepareStatement(query2);
+        ps1.setString(1, full_name);
+        ps1.setString(2, address);
+        ps1.setDate(3, sqlDate);
+        ps1.setString(4, phone);
+        ps1.setInt(5, classroom_certified);
+        ps1.setInt(6, dirtbike_certified);
+        ps1.setInt(7, streetbike_certified);
+        try {
+            if (ps.executeUpdate() > 0 && ps1.executeUpdate() > 0) {
+                conn.commit();
+                System.out.println("SUCCESS");
+            }
+        } catch (SQLException e) {
+            conn.rollback();
+            e.printStackTrace();
+        }
     }
 
 }
